b0c5e1ad66f84c009bd7c3231ad5187697c0a5f1
Ole Sørensen <ole.sorensen@visma.com>
2017-11-09 14:43:03 +0100

    Implementing the DashboardViewModelBuilder

--- OSDevGrp.MyDashboard.Web.Tests/Factories/DashboardViewModelBuilder/BuildAsyncTests.cs
+++ OSDevGrp.MyDashboard.Web.Tests/Factories/DashboardViewModelBuilder/BuildAsyncTests.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using OSDevGrp.MyDashboard.Core.Contracts.Models;
 using OSDevGrp.MyDashboard.Core.Tests.Helpers.Attributes;
 using OSDevGrp.MyDashboard.Web.Contracts.Factories;
+using OSDevGrp.MyDashboard.Web.Contracts.Helpers;
 using OSDevGrp.MyDashboard.Web.Models;
 
 namespace OSDevGrp.MyDashboard.Web.Tests.Factories.DashboardViewModelBuilder
@@ -9,6 +15,24 @@ namespace OSDevGrp.MyDashboard.Web.Tests.Factories.DashboardViewModelBuilder
     [TestClass]
     public class BuildAsyncTests
     {
+        #region Private variables
+
+        private Mock<IViewModelBuilder<InformationViewModel, INews>> _newsToInformationViewModelBuilderMock;
+        private Mock<IViewModelBuilder<SystemErrorViewModel, ISystemError>> _systemErrorViewModelBuilderMock;
+        private Mock<IHtmlHelper> _htmlHelperMock;
+        private Random _random;
+
+        #endregion
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _newsToInformationViewModelBuilderMock = new Mock<IViewModelBuilder<InformationViewModel, INews>>();
+            _systemErrorViewModelBuilderMock = new Mock<IViewModelBuilder<SystemErrorViewModel, ISystemError>>();
+            _htmlHelperMock = new Mock<IHtmlHelper>();
+            _random = new Random(DateTime.Now.Millisecond);
+        }
+
         [TestMethod]
         [ExpectedArgumentNullExceptionAttribute("input")]
         public void BuildAsync_WhenDashboardIsNull_ThrowsArgumentNullException()
@@ -18,9 +42,236 @@ namespace OSDevGrp.MyDashboard.Web.Tests.Factories.DashboardViewModelBuilder
             sut.BuildAsync(null);
         }
 
-        private IViewModelBuilder<DashboardViewModel, IDashboard> CreateSut()
+        [TestMethod]
+        public void BuildAsync_WhenCalled_AssertNewsWasCalledOnDashboardTwice()
         {
-            return new OSDevGrp.MyDashboard.Web.Factories.DashboardViewModelBuilder();
+            Mock<IDashboard> dashboardMock = CreateDashboardMock();
+
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut();
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboardMock.Object);
+            buildTask.Wait();
+
+            dashboardMock.Verify(m => m.News, Times.Exactly(2));
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalled_AssertSystemErrorsWasCalledOnDashboardTwice()
+        {
+            Mock<IDashboard> dashboardMock = CreateDashboardMock();
+
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut();
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboardMock.Object);
+            buildTask.Wait();
+
+            dashboardMock.Verify(m => m.SystemErrors, Times.Exactly(2));
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalled_AssertBuildAsyncWasCalledOnNewsToInformationViewModelBuilderForEachNewsInDashboard()
+        {
+            List<INews> newsCollection = CreateNewsCollection(_random.Next(50, 75)).ToList();
+            IDashboard dashboard = CreateDashboard(newsCollection: newsCollection);
+
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut();
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboard);
+            buildTask.Wait();
+
+            newsCollection.ForEach(news => _newsToInformationViewModelBuilderMock.Verify(m => m.BuildAsync(It.Is<INews>(value => value == news)), Times.Once));
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalled_AssertBuildAsyncWasCalledOnSystemErrorViewModelBuilderForEachSystemErrorInDashboard()
+        {
+            List<ISystemError> systemErrorCollection = CreateSystemErrorCollection(_random.Next(50, 75)).ToList();
+            IDashboard dashboard = CreateDashboard(systemErrorCollection: systemErrorCollection);
+
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut();
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboard);
+            buildTask.Wait();
+
+            systemErrorCollection.ForEach(systemError => _systemErrorViewModelBuilderMock.Verify(m => m.BuildAsync(It.Is<ISystemError>(value => value == systemError)), Times.Once));
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalled_ReturnsInitializedDashboardViewModel()
+        {
+            List<INews> newsCollection = CreateNewsCollection(_random.Next(50, 75)).ToList();
+            List<ISystemError> systemErrorCollection = CreateSystemErrorCollection(_random.Next(10, 15)).ToList();
+            IDashboard dashboard = CreateDashboard(newsCollection: newsCollection, systemErrorCollection: systemErrorCollection);
+
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut();
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboard);
+            buildTask.Wait();
+
+            DashboardViewModel result = buildTask.Result;
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Informations);
+            Assert.AreEqual(newsCollection.Count, result.Informations.Count());
+            Assert.IsNotNull(result.SystemErrors);
+            Assert.AreEqual(systemErrorCollection.Count, result.SystemErrors.Count());
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalledAndNewsToInformationViewModelBuilderThrowsAggregateException_AddsExceptionToSystemViewModelsInDashboardViewModel()
+        {
+            IEnumerable<INews> newsCollection = CreateNewsCollection(1);
+            IEnumerable<ISystemError> systemErrorCollection = CreateSystemErrorCollection(0);
+            IDashboard dashboard = CreateDashboard(newsCollection: newsCollection, systemErrorCollection: systemErrorCollection);
+
+            string aggregateExceptionMessage = Guid.NewGuid().ToString();
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut(aggregateExceptionMessage: aggregateExceptionMessage);
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboard);
+            buildTask.Wait();
+
+            DashboardViewModel result = buildTask.Result;
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Informations);
+            Assert.AreEqual(0, result.Informations.Count());
+            Assert.IsNotNull(result.SystemErrors);
+            Assert.AreEqual(1, result.SystemErrors.Count());
+            
+            SystemErrorViewModel systemErrorViewModel = result.SystemErrors.First();
+            Assert.IsNotNull(systemErrorViewModel);
+            Assert.IsNotNull(systemErrorViewModel.SystemErrorIdentifier);
+            Assert.IsTrue(systemErrorViewModel.Timestamp >= DateTime.Now.AddSeconds(-3) && systemErrorViewModel.Timestamp <= DateTime.Now);
+            Assert.IsNotNull(systemErrorViewModel.Message);
+            Assert.AreEqual($"HtmlHelper.ConvertNewLines:{aggregateExceptionMessage}", systemErrorViewModel.Message);
+            Assert.IsNotNull(systemErrorViewModel.Details);
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalledAndSystemErrorViewModelBuilderThrowsAggregateException_AddsExceptionToSystemViewModelsInDashboardViewModel()
+        {
+            IEnumerable<INews> newsCollection = CreateNewsCollection(0);
+            IEnumerable<ISystemError> systemErrorCollection = CreateSystemErrorCollection(1);
+            IDashboard dashboard = CreateDashboard(newsCollection: newsCollection, systemErrorCollection: systemErrorCollection);
+
+            string aggregateExceptionMessage = Guid.NewGuid().ToString();
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut(aggregateExceptionMessage: aggregateExceptionMessage);
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboard);
+            buildTask.Wait();
+
+            DashboardViewModel result = buildTask.Result;
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Informations);
+            Assert.AreEqual(0, result.Informations.Count());
+            Assert.IsNotNull(result.SystemErrors);
+            Assert.AreEqual(1, result.SystemErrors.Count());
+            
+            SystemErrorViewModel systemErrorViewModel = result.SystemErrors.First();
+            Assert.IsNotNull(systemErrorViewModel);
+            Assert.IsNotNull(systemErrorViewModel.SystemErrorIdentifier);
+            Assert.IsTrue(systemErrorViewModel.Timestamp >= DateTime.Now.AddSeconds(-3) && systemErrorViewModel.Timestamp <= DateTime.Now);
+            Assert.IsNotNull(systemErrorViewModel.Message);
+            Assert.AreEqual($"HtmlHelper.ConvertNewLines:{aggregateExceptionMessage}", systemErrorViewModel.Message);
+            Assert.IsNotNull(systemErrorViewModel.Details);
+        }
+
+        [TestMethod]
+        public void BuildAsync_WhenCalledAndAggregateExceptionOccurs_AssertConvertNewLinesWasCalledOnHtmlHelperTwice()
+        {
+            IEnumerable<INews> newsCollection = CreateNewsCollection(1);
+            IEnumerable<ISystemError> systemErrorCollection = CreateSystemErrorCollection(0);
+            IDashboard dashboard = CreateDashboard(newsCollection: newsCollection, systemErrorCollection: systemErrorCollection);
+
+            string aggregateExceptionMessage = Guid.NewGuid().ToString();
+            IViewModelBuilder<DashboardViewModel, IDashboard> sut = CreateSut(aggregateExceptionMessage: aggregateExceptionMessage);
+
+            Task<DashboardViewModel> buildTask = sut.BuildAsync(dashboard);
+            buildTask.Wait();
+
+            _htmlHelperMock.Verify(m => m.ConvertNewLines(It.IsAny<string>()), Times.Exactly(2));
+        }
+
+        private IViewModelBuilder<DashboardViewModel, IDashboard> CreateSut(string aggregateExceptionMessage = null)
+        {
+            _newsToInformationViewModelBuilderMock.Setup(m => m.BuildAsync(It.IsAny<INews>()))
+                .Returns(Task.Run<InformationViewModel>(() => 
+                {
+                    if (string.IsNullOrWhiteSpace(aggregateExceptionMessage) == false)
+                    {
+                        throw new Exception(aggregateExceptionMessage);
+                    }
+                    return CreateInformationViewModel(DateTime.Now.AddMinutes(_random.Next(1, 30) * -1));
+                }));
+
+            _systemErrorViewModelBuilderMock.Setup(m => m.BuildAsync(It.IsAny<ISystemError>()))
+                .Returns(Task.Run<SystemErrorViewModel>(() => 
+                {
+                    if (string.IsNullOrWhiteSpace(aggregateExceptionMessage) == false)
+                    {
+                        throw new Exception(aggregateExceptionMessage);
+                    }
+                    return CreateSystemErrorViewModel(DateTime.Now.AddMinutes(_random.Next(1, 30) * -1));
+                }));
+
+            _htmlHelperMock.Setup(m => m.ConvertNewLines(It.IsAny<string>()))
+                .Returns<string>(value => $"HtmlHelper.ConvertNewLines:{value}");
+
+            return new OSDevGrp.MyDashboard.Web.Factories.DashboardViewModelBuilder(
+                _newsToInformationViewModelBuilderMock.Object,
+                _systemErrorViewModelBuilderMock.Object,
+                _htmlHelperMock.Object);
+        }
+
+        private IDashboard CreateDashboard(IEnumerable<INews> newsCollection = null, IEnumerable<ISystemError> systemErrorCollection = null)
+        {
+            return CreateDashboardMock(newsCollection, systemErrorCollection).Object;
+        }
+
+        private Mock<IDashboard> CreateDashboardMock(IEnumerable<INews> newsCollection = null, IEnumerable<ISystemError> systemErrorCollection = null)
+        {
+            Mock<IDashboard> dashboardMock = new Mock<IDashboard>();
+            dashboardMock.Setup(m => m.News)
+                .Returns(newsCollection ?? CreateNewsCollection(_random.Next(50, 100)));
+            dashboardMock.Setup(m => m.SystemErrors)
+                .Returns(systemErrorCollection ?? CreateSystemErrorCollection(_random.Next(5, 10)));
+            return dashboardMock;
+        }
+
+        private IEnumerable<INews> CreateNewsCollection(int numberOfNews)
+        {
+            IList<INews> newsCollection = new List<INews>(numberOfNews);
+            while (newsCollection.Count < numberOfNews)
+            {
+                Mock<INews> newsMock = new Mock<INews>();
+                newsCollection.Add(newsMock.Object);
+            }
+            return newsCollection;
+        }
+
+        private IEnumerable<ISystemError> CreateSystemErrorCollection(int numberOfSystemErrors)
+        {
+            IList<ISystemError> systemErrorCollection = new List<ISystemError>(numberOfSystemErrors);
+            while (systemErrorCollection.Count < numberOfSystemErrors)
+            {
+                Mock<ISystemError> systemErrorMock = new Mock<ISystemError>();
+                systemErrorCollection.Add(systemErrorMock.Object);
+            }
+            return systemErrorCollection;
+        }
+
+        private InformationViewModel CreateInformationViewModel(DateTime timestamp)
+        {
+            return new InformationViewModel
+            {
+                Timestamp = timestamp
+            };
+        }
+
+        private SystemErrorViewModel CreateSystemErrorViewModel(DateTime timestamp)
+        {
+            return new SystemErrorViewModel
+            {
+                Timestamp = timestamp
+            };
         }
     } 
 }
--- OSDevGrp.MyDashboard.Web/Factories/DashboardViewModelBuilder.cs
+++ OSDevGrp.MyDashboard.Web/Factories/DashboardViewModelBuilder.cs
@@ -1,16 +1,222 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using OSDevGrp.MyDashboard.Core.Contracts.Models;
+using OSDevGrp.MyDashboard.Web.Contracts.Factories;
+using OSDevGrp.MyDashboard.Web.Contracts.Helpers;
+using OSDevGrp.MyDashboard.Web.Contracts.Models;
 using OSDevGrp.MyDashboard.Web.Models;
 
 namespace OSDevGrp.MyDashboard.Web.Factories
 {
     public class DashboardViewModelBuilder : ViewModelBuilderBase<DashboardViewModel, IDashboard>
     {
+        #region Private variables
+
+        private readonly IViewModelBuilder<InformationViewModel, INews> _newsToInformationViewModelBuilder;
+        private readonly IViewModelBuilder<SystemErrorViewModel, ISystemError> _systemErrorViewModelBuilder;
+        private readonly IHtmlHelper _htmlHelper;
+
+        #endregion
+
+        #region Constructor
+
+        public DashboardViewModelBuilder(IViewModelBuilder<InformationViewModel, INews> newsToInformationViewModelBuilder, IViewModelBuilder<SystemErrorViewModel, ISystemError> systemErrorViewModelBuilder, IHtmlHelper htmlHelper)
+        {
+            if (newsToInformationViewModelBuilder == null) throw new ArgumentNullException(nameof(newsToInformationViewModelBuilder));
+            if (systemErrorViewModelBuilder == null) throw new ArgumentNullException(nameof(systemErrorViewModelBuilder));
+            if (htmlHelper == null) throw new ArgumentNullException(nameof(htmlHelper));
+
+            _newsToInformationViewModelBuilder = newsToInformationViewModelBuilder;
+            _systemErrorViewModelBuilder = systemErrorViewModelBuilder;
+            _htmlHelper = htmlHelper;
+        }
+
+        #endregion
+
         #region Methods
 
         protected override DashboardViewModel Build(IDashboard dashboard)
         {
-            throw new NotImplementedException();
+            IList<InformationViewModel> informationViewModels = new List<InformationViewModel>();
+            IList<SystemErrorViewModel> systemErrorViewModels = new List<SystemErrorViewModel>();
+            object syncRoot = new object();
+
+            try
+            {
+                Task handleInformationViewModelsTask = HandleAsync<InformationViewModel>(
+                    dashboard, 
+                    GenerateInformationViewModelBuilderTaskArray,
+                    informationViewModel => AddInformationViewModel(informationViewModel, informationViewModels, syncRoot),
+                    exception => HandleException(exception, systemErrorViewModels, syncRoot));
+                Task handleSystemErrorViewModelsTask = HandleAsync<SystemErrorViewModel>(
+                    dashboard, 
+                    GenerateSystemErrorViewModelBuilderTaskArray,
+                    systemErrorViewModel => AddSystemErrorViewModel(systemErrorViewModel, systemErrorViewModels, syncRoot),
+                    exception => HandleException(exception, systemErrorViewModels, syncRoot));
+                Task.WaitAll(new [] {handleInformationViewModelsTask, handleSystemErrorViewModelsTask});
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex, systemErrorViewModels, syncRoot);
+            }
+            
+            return new DashboardViewModel
+            {
+                Informations = informationViewModels.OrderByDescending(informationViewModel => informationViewModel.Timestamp),
+                SystemErrors = systemErrorViewModels.OrderByDescending(systemErrorViewModel => systemErrorViewModel.Timestamp)
+            };
+        }
+
+        private Task<InformationViewModel>[] GenerateInformationViewModelBuilderTaskArray(IDashboard dashboard)
+        {
+            if (dashboard == null)
+            {
+                throw new ArgumentNullException(nameof(dashboard));
+            }
+
+            if (dashboard.News == null)
+            {
+                return new Task<InformationViewModel>[0];
+            }
+
+            return dashboard.News
+                .Select(news => _newsToInformationViewModelBuilder.BuildAsync(news))
+                .ToArray();
+        }
+
+        private Task<SystemErrorViewModel>[] GenerateSystemErrorViewModelBuilderTaskArray(IDashboard dashboard)
+        {
+            if (dashboard == null)
+            {
+                throw new ArgumentNullException(nameof(dashboard));
+            }
+
+            if (dashboard.SystemErrors == null)
+            {
+                return new Task<SystemErrorViewModel>[0];
+            }
+
+            return dashboard.SystemErrors
+                .Select(systemError => _systemErrorViewModelBuilder.BuildAsync(systemError))
+                .ToArray();
+        }
+
+        private Task HandleAsync<TViewModel>(IDashboard dashboard, Func<IDashboard, Task<TViewModel>[]> taskArrayGenerator, Action<TViewModel> resultHandler, Action<Exception> exceptionHandler) where TViewModel : IViewModel
+        {
+            if (dashboard == null)
+            {
+                throw new ArgumentNullException(nameof(dashboard));
+            }
+            if (taskArrayGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(taskArrayGenerator));
+            }
+            if (resultHandler == null)
+            {
+                throw new ArgumentNullException(nameof(resultHandler));
+            }
+            if (exceptionHandler == null)
+            {
+                throw new ArgumentNullException(nameof(exceptionHandler));
+            }
+
+            return Task.Run(() => 
+            {
+                try
+                {
+                    Task<TViewModel>[] taskArray = taskArrayGenerator(dashboard);
+                    Task.WaitAll(taskArray);
+
+                    foreach (TViewModel viewModel in taskArray.Where(task => task.IsCompletedSuccessfully).Select(task => task.Result))
+                    {
+                        resultHandler(viewModel);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exceptionHandler(ex);
+                }
+            });
+        }
+
+        private void HandleException(Exception exception, IList<SystemErrorViewModel> systemErrorViewModelCollection, object syncRoot)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+            if (systemErrorViewModelCollection == null)
+            {
+                throw new ArgumentNullException(nameof(systemErrorViewModelCollection));
+            }
+            if (syncRoot == null)
+            {
+                throw new ArgumentNullException(nameof(syncRoot));
+            }
+
+            AggregateException aggregateException = exception as AggregateException;
+            if (aggregateException != null)
+            {
+                aggregateException.Handle(ex => 
+                {
+                    HandleException(ex, systemErrorViewModelCollection, syncRoot);
+                    return true;
+                });
+                return;
+            }
+
+            SystemErrorViewModel systemErrorViewModel = new SystemErrorViewModel
+            {
+                SystemErrorIdentifier = Guid.NewGuid().ToString(),
+                Timestamp = DateTime.Now,
+                Message = _htmlHelper.ConvertNewLines(exception.Message),
+                Details = _htmlHelper.ConvertNewLines(exception.StackTrace)
+            };
+            AddSystemErrorViewModel(systemErrorViewModel, systemErrorViewModelCollection, syncRoot);
+        }
+
+        private static void AddInformationViewModel(InformationViewModel informationViewModel, IList<InformationViewModel> informationViewModelCollection, object syncRoot)
+        {
+            if (informationViewModel == null)
+            {
+                throw new ArgumentNullException(nameof(informationViewModel));
+            }
+            if (informationViewModelCollection == null)
+            {
+                throw new ArgumentNullException(nameof(informationViewModelCollection));
+            }
+            if (syncRoot == null)
+            {
+                throw new ArgumentNullException(nameof(syncRoot));
+            }
+
+            lock (syncRoot)
+            {
+                informationViewModelCollection.Add(informationViewModel);
+            }
+        }
+
+        private static void AddSystemErrorViewModel(SystemErrorViewModel systemErrorViewModel, IList<SystemErrorViewModel> systemErrorViewModelCollection, object syncRoot)
+        {
+            if (systemErrorViewModel == null)
+            {
+                throw new ArgumentNullException(nameof(systemErrorViewModel));
+            }
+            if (systemErrorViewModelCollection == null)
+            {
+                throw new ArgumentNullException(nameof(systemErrorViewModelCollection));
+            }
+            if (syncRoot == null)
+            {
+                throw new ArgumentNullException(nameof(syncRoot));
+            }
+
+            lock (syncRoot)
+            {
+                systemErrorViewModelCollection.Add(systemErrorViewModel);
+            }
         }
 
         #endregion
