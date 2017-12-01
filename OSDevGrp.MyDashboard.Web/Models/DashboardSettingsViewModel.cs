31d197762aa479d00a3ba9da60297539e9433098
Ole Sørensen <ole.sorensen@visma.com>
2017-12-01 12:54:41 +0100

    Adding view for the dashboard settings

--- OSDevGrp.MyDashboard.Web/Models/DashboardSettingsViewModel.cs
+++ OSDevGrp.MyDashboard.Web/Models/DashboardSettingsViewModel.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using OSDevGrp.MyDashboard.Web.Contracts.Models;
 
 namespace OSDevGrp.MyDashboard.Web.Models
 {
     public class DashboardSettingsViewModel : IViewModel
     {
+        [Range(0, 250)]
+        [Display(Name="Number of news", ShortName="News", Description="Number of news to receive")]
         public int NumberOfNews { get; set; }
     }
 }
